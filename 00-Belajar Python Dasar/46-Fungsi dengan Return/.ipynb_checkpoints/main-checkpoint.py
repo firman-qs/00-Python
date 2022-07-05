18e0072ff48b87802bd6bda9fc8bf4c0b542ba0d
fqsDev <42881702+firman-qs@users.noreply.github.com>
2022-07-05 22:54:58 +0700

    Fungsi dengan Return ipynb

---
+++ 00-Belajar Python Dasar/46-Fungsi dengan Return/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,16 @@
+# Fungsi dengan return value
+def kuadrat(x):
+    return x ** 2
+
+print(kuadrat(2))
+
+print("-" * 10)
+
+
+# Fungsi dengan return value dan multiple argumen
+def tambahXY(x, y):
+    return x + y
+
+hasil = tambahXY(2, 3)
+print(hasil)
+
