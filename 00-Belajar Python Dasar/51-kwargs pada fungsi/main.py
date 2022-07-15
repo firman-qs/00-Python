6ec5830e9bb8d4e534fbe694342724c6de6ed22f
fqsDev <42881702+firman-qs@users.noreply.github.com>
2022-07-15 13:20:19 +0700

    menambahkan 51-kwargs pada fungsi

---
+++ 00-Belajar Python Dasar/51-kwargs pada fungsi/main.py
@@ -0,0 +1,39 @@
+from ast import arg
+import os
+os.system('cls')
+
+# **kwargs pada fungsi
+
+def fungsi(nama, tinggi, berat):
+    '''regular function'''
+    print(f"{nama} punya tinggi {tinggi} dan berat {berat}")
+
+fungsi('pudge',110,100)
+
+
+
+def fungsi(**kwargs):
+    '''regular function'''
+    print(f"{kwargs['nama']} punya tinggi {kwargs['tinggi']} dan berat {kwargs['berat']}")
+
+fungsi(nama="pudge", tinggi=110, berat=100)
+
+
+#contoh
+def math(*args, **kwargs):
+    if kwargs['operasi'] == 'tambah':
+        print("Operasi Penjumlahan")
+        hasil = args[0]
+        for number in args[1:]:
+            hasil += number
+        print(hasil)
+        
+    elif kwargs['operasi'] == 'kali':
+        print("Operasi Perkalian")
+        hasil = args[0]
+        for number in args[1:]:
+            hasil *= number
+        print(hasil)
+    
+    
+math(2,2,3, operasi = 'tambah')
