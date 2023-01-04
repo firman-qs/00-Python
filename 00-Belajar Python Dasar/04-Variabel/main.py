32e2605394f6c2e9be52c48fc60ac579723c3a11
firman-qs <firmansabil10@gmail.com>
2023-01-04 09:34:18 +0700

    menambahkan 31 s.d. 35 baru

--- 00-Belajar Python Dasar/04-Variabel/main.py
+++ 00-Belajar Python Dasar/04-Variabel/main.py
@@ -1,28 +1,27 @@
 # Variabel adalah tempat untuk menyimpan data
 
 # assignment
-a   = 10
-b   = 15
+a = 10
+b = 15
 phi = 3.14
 pHi = 314
-
 # pemanggilan pertama
 print("==========={ pemanggilan pertama  }==========")
-print("Nilai a =",a)
-print("Nilai a =",phi)
-print("Nilai a =",pHi)
-print("==========={ pemanggilan kedua    }==========")
+print("Nilai a =", a)
+print("Nilai a =", phi)
+print("Nilai a =", pHi)
+print("==========={ pemanggilan kedua }==========")
 
 # penamaan
-nilai_y = 20 # dengan menggunakan underscore
-juta10  = 1000000 # angka tidak boleh didepan 
-nilaiZ  = 12 # ini boleh
+nilai_y = 20  # dengan menggunakan underscore
+juta10 = 1000000  # angka tidak boleh didepan
+nilaiZ = 12  # ini boleh
 
 # pemanggilan kedua
-a   = 6
-print("Nilai a =",a)
+a = 6
+print("Nilai a =", a)
 print("=============================================")
 
 # assignment indirect
 a = (b+nilai_y)
-print("Nilai a =",a)
+print("Nilai a =", a)
